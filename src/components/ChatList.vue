00c035b7957692efab67038c92480c09a13e64f5
ponochovny <ponochovny@gmail.com>
2022-07-21 15:13:38 +0300

    subscribe to the chats collection

--- src/components/ChatList.vue
+++ src/components/ChatList.vue
@@ -1,14 +1,44 @@
 <template>
 	<div>
+		<ul>
+			<li v-for="chat of chats" :key="chat.id">
+				{{ chat.id }}
+			</li>
+		</ul>
 		<button @click="createChatRoom()">Create New Chat Room</button>
 	</div>
 </template>
 
 <script>
 import { db } from '../firebase'
-import { collection, addDoc } from 'firebase/firestore'
+import {
+	collection,
+	addDoc,
+	query,
+	where,
+	onSnapshot,
+} from 'firebase/firestore'
 
 export default {
+	data() {
+		return {
+			chats: [],
+			unsubscribe: () => {},
+		}
+	},
+	created() {
+		const q = query(collection(db, 'chats'), where('owner', '==', this.uid))
+		this.unsubscribe = onSnapshot(q, (querySnapshot) => {
+			const chats = []
+			querySnapshot.forEach((doc) => {
+				chats.push({ id: doc.id, ...doc.data() })
+			})
+			this.chats = chats
+		})
+	},
+	unmounted() {
+		this.unsubscribe()
+	},
 	methods: {
 		async createChatRoom() {
 			const colRef = collection(db, 'chats')
