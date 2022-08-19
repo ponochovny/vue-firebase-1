b0bab033ae411d9b9fcebdd88cd7a35cc316b7af
ponochovny <ponochovny@gmail.com>
2022-08-19 17:23:09 +0300

    fixes

--- src/components/ChatList.vue
+++ src/components/ChatList.vue
@@ -1,8 +1,19 @@
 <template>
-	<div>
-		<div class="list-group my-4" v-if="chats.length > 0">
+	<div class="mt-4">
+		<p class="fs-5">Other Chats</p>
+		<div class="list-group mb-3" v-if="otherChats.length > 0">
 			<router-link
-				v-for="chat of chats"
+				v-for="chat of otherChats"
+				:key="chat.id"
+				:to="{ name: 'chat', params: { id: chat.id } }"
+				class="list-group-item list-group-item-action"
+				>{{ chat.id }}</router-link
+			>
+		</div>
+		<p class="fs-5">Owned Chats</p>
+		<div class="list-group mb-4" v-if="ownedChats.length > 0">
+			<router-link
+				v-for="chat of ownedChats"
 				:key="chat.id"
 				:to="{ name: 'chat', params: { id: chat.id } }"
 				class="list-group-item list-group-item-action"
@@ -10,7 +21,7 @@
 			>
 		</div>
 
-		<p v-else class="mb-4 mt-4 fs-5">Currently, You have no active chats</p>
+		<p v-else class="mb-4 mt-2 fs-6">Currently, You have no active chats</p>
 
 		<button @click="createChatRoom()" class="btn btn-success">
 			Create New Chat Room
@@ -31,18 +42,31 @@ import {
 export default {
 	data() {
 		return {
-			chats: [],
+			ownedChats: [],
+			otherChats: [],
 			unsubscribe: () => {},
+			unsubscribe2: () => {},
 		}
 	},
 	created() {
 		const q = query(collection(db, 'chats'), where('owner', '==', this.uid))
 		this.unsubscribe = onSnapshot(q, (querySnapshot) => {
-			const chats = []
+			const ownedChats = []
 			querySnapshot.forEach((doc) => {
-				chats.push({ id: doc.id, ...doc.data() })
+				ownedChats.push({ id: doc.id, ...doc.data() })
 			})
-			this.chats = chats
+			this.ownedChats = ownedChats
+		})
+
+		//
+
+		const q2 = query(collection(db, 'chats'), where('owner', '!=', this.uid))
+		this.unsubscribe2 = onSnapshot(q2, (querySnapshot) => {
+			const otherChats = []
+			querySnapshot.forEach((doc) => {
+				otherChats.push({ id: doc.id, ...doc.data() })
+			})
+			this.otherChats = otherChats
 		})
 	},
 	unmounted() {
